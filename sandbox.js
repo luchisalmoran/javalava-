ac1cfd9b65a4219844c06ee3019111f6748d14d7
lucy <56303888+luchisalmoran@users.noreply.github.com>
2021-11-18 09:30:01 -0500

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,25 @@
+//string
+console.log('hello world');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+//string concatenation
+let firstname = 'Brandon';
+let lastname = 'sanders';
+
+let fullname = firstname + '' + lastname;
+console.log(fullname);
+
+//getting characters
+console.log(fullname[2]);
+
+//string length
+console.log(fullname.length);
+
+//string methods
+console.log(fullname.toUpperCase())
+let result = fullname.toLowerCase();
+console.log(result);
+let index = email.indexOf('@')
+console.log(index);
